decee024dbe5a519dea4b11f934ae73a2278eafd
foresle <foresle@tutanota.com>
2021-06-05 16:14:29 +0300

    Add a first interface

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import os
+import time
 from xml.etree import ElementTree
 from PIL import Image
-import cv2
+# import cv2
+from slidecrea import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+# import sys
 
 
-def create_img_xml(path_to_img, duration=15):
+def create_img_xml(path_to_img, progress_bar, duration=15):
+    path_to_img = path_to_img + '/'
     images_list = []
 
     for obj in os.listdir(path_to_img):
@@ -15,6 +20,10 @@ def create_img_xml(path_to_img, duration=15):
 
     xml_parent = ElementTree.Element('background')
 
+    progres_value = 1
+    progress_bar.setValue(progres_value)
+    progres_deltax = int(100/len(images_list))
+
     for image in images_list:
         xml_static = ElementTree.SubElement(xml_parent, 'static')
         xml_static_duration = ElementTree.SubElement(xml_static, 'duration')
@@ -22,8 +31,14 @@ def create_img_xml(path_to_img, duration=15):
         xml_static_duration.text = str(duration) + '.0' if duration == int(duration) else str(duration)
         xml_static_file.text = str(os.path.abspath(image))
 
+        progres_value += progres_deltax
+        progress_bar.setValue(progres_value)
+
     xml_tree = ElementTree.ElementTree(xml_parent)
     xml_tree.write(path_to_img + 'wallpapers_list.xml')
+
+    progres_value = 100
+    progress_bar.setValue(progres_value)
 
 
 def create_gif_xml(path_to_gif):
@@ -40,27 +55,61 @@ def create_gif_xml(path_to_gif):
     create_img_xml(path_to_frames_folder + r'/', 0.5)
 
 
-def create_mp4_xml(path_to_mp4):
-    path_to_frames_folder = path_to_mp4[:-(len(os.path.basename(path_to_mp4)))] + os.path.basename(path_to_mp4)[:-4]
+#
+# def create_mp4_xml(path_to_mp4):
+#     path_to_frames_folder = path_to_mp4[:-(len(os.path.basename(path_to_mp4)))] + os.path.basename(path_to_mp4)[:-4]
+#
+#     os.mkdir(path_to_frames_folder)
+#
+#     mp4_cap = cv2.VideoCapture(path_to_mp4)
+#     frame_count = int(mp4_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+#
+#     for frame in range(frame_count):
+#         ret, frame_image = mp4_cap.read()
+#         if frame % 16 == 0:
+#             cv2.imwrite(path_to_frames_folder + f'/{frame}.png', frame_image)
+#
+#     mp4_cap.release()
+#
+#     create_img_xml(path_to_frames_folder + r'/', 0.5)
+#
 
-    os.mkdir(path_to_frames_folder)
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
-    mp4_cap = cv2.VideoCapture(path_to_mp4)
-    frame_count = int(mp4_cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
-    for frame in range(frame_count):
-        ret, frame_image = mp4_cap.read()
-        if frame % 16 == 0:
-            cv2.imwrite(path_to_frames_folder + f'/{frame}.png', frame_image)
+        # Ініцілізація кнопок
+        self.selectImagesPathButton.clicked.connect(self.select_images_path_button)
+        self.createXMLImagesFileButton.clicked.connect(self.create_xml_images_file_button)
 
-    mp4_cap.release()
+    def select_images_path_button(self):
+        self.lineEditImagesPath.setText(str(QtWidgets.QFileDialog.getExistingDirectory()))
 
-    create_img_xml(path_to_frames_folder + r'/', 0.5)
+    def create_xml_images_file_button(self):
+        self.createXMLImagesFileButton.setEnabled(False)
+        self.selectImagesPathButton.setEnabled(False)
+        create_img_xml(self.lineEditImagesPath.text(), self.progressBarIImagesXML)
+        time.sleep(1)
+        self.createXMLImagesFileButton.setEnabled(True)
+        self.selectImagesPathButton.setEnabled(True)
+        self.progressBarIImagesXML.setValue(0)
 
 
-DEFAULT_PATH_TO_IMG = r'../../Pictures/Walls/'
-DEFAULT_PATH_TO_GIF = r'../../Pictures/Walls/skelets.gif'
-DEFAULT_PATH_TO_MP4 = r'../../Pictures/Walls/Pexels Videos 1654216.mp4'
+# DEFAULT_PATH_TO_IMG = r'../../Pictures/Walls/'кк
+# DEFAULT_PATH_TO_GIF = r'../../Pictures/Walls/skelets.gif'
+# DEFAULT_PATH_TO_MP4 = r'../../Pictures/Walls/Pexels Videos 1654216.mp4'
 # create_img_xml(DEFAULT_PATH_TO_IMG)
 # create_gif_xml(DEFAULT_PATH_TO_GIF)
-create_mp4_xml(DEFAULT_PATH_TO_MP4)
+# create_mp4_xml(DEFAULT_PATH_TO_MP4)
+
+
+if __name__ == "__main__":
+    import sys
+
+    # Ініцілізуємо та запустимо головне вікно
+    app = QtWidgets.QApplication(sys.argv)
+    appMainWindow = MainWindow()
+    appMainWindow.show()
+    sys.exit(app.exec_())
